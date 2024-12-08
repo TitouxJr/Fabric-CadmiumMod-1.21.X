9cc177ecb523517e1dfb787f8f9bfb65b05c1c56
TitouxJr <111535159+TitouxJr@users.noreply.github.com>
2024-12-08 12:11:31 +0100

    Initial commit

---
+++ src/main/java/net/titoux/cadmiummod/CadmiumMod.java
@@ -0,0 +1,16 @@
+package net.titoux.cadmiummod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CadmiumMod implements ModInitializer {
+	public static final String MOD_ID = "cadmium-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/titoux/cadmiummod/CadmiumModClient.java
@@ -0,0 +1,10 @@
+package net.titoux.cadmiummod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CadmiumModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
